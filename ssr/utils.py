c074c4162f99207b67bea2574d8aac3b5fbb3035
jolsten <jolsten@gmail.com>
2023-08-19 08:17:35 -0400

    tweak

--- ssr/utils.py
+++ ssr/utils.py
@@ -5,7 +5,7 @@ A5_TO_CHAR = {c: i for i, c in CHAR_TO_A5}
 
 
 def catalog_number_to_alpha5(v: int) -> str:
-    
+    return ''
 
 def alpha5_to_catalog_number(v: str) -> int:
     return v[0] * 10000 + int(v[1:5])
