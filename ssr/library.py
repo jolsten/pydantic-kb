fda8d33179a6640212d2a3b18b60aaaeaedf75a4
Jonathan Olsten <jolsten@gmail.com>
2023-08-20 18:50:48 -0400

    added a Library class

--- ssr/library.py
+++ ssr/library.py
@@ -1,9 +1,12 @@
 import pathlib
-from typing import Optional, Union, Tuple, Dict
+from typing import Optional, Union, Tuple, Dict, Literal
 from .models.base import BaseModel
 from .yaml import load_yaml
 from .config import load_settings
 from .typing import PathLike
+
+# Category = Literal
+# Category.__args__ = (cls.__name__ for cls in BaseModel.__subclasses__())
 
 class Record(BaseModel):
     source: PathLike
@@ -27,14 +30,21 @@ class Library:
             sequence = load_yaml(file.read_text())
             for item in sequence:
                 cls_name = item.__class__.__name__
-                self._items[cls_name.lower()][item.name.lower()] = Record(source=file, object=item)
-    
-    def get_item(self, _cls: Union[str, BaseModel], name: str, /) -> BaseModel:
-        return self._items[_cls.lower()][name.lower()].object
-    
+                self[cls_name, item.name] = Record(source=file, object=item)
+
     def get_category(self, _cls: Union[str, BaseModel], /) -> Dict[str, BaseModel]:
         records = self._items[_cls.lower()]
         return {name: rec.object for name, rec in records.items()}
+
+    def get_item(self, _cls: Union[str, BaseModel], name: str, /) -> BaseModel:
+        return self.get_category(_cls)[name.lower()]
+
+    def __setitem__(self, key: Tuple[str], value) -> None:
+        if isinstance(key, tuple):
+            _cls, name = key
+            self._items[_cls.lower()][name.lower()] = value
+        else:
+            raise ValueError(f'Library.__setitem__() expects a two-element tuple but got {key!r}')
 
     def __getitem__(self, key: Union[str, Tuple[str]]) -> dict:
         if isinstance(key, tuple):
---
+++ tests/test_library.py
@@ -0,0 +1,5 @@
+import pytest
+from ssr import Library
+
+def test_load_library():
+    lib = Library()
